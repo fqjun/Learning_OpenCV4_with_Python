cded6934f124dbb6246dab2b7fdf1699f8279eff
linyuxuanlin <me@wiki-power.com>
2021-05-07 11:31:27 +0800

    1

--- Reference/Chapter06/01-chapter-content/grayscale_histogram.py
+++ Reference/Chapter06/01-chapter-content/grayscale_histogram.py
@@ -7,6 +7,9 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
+import os
+dirname, filename = os.path.split(os.path.abspath( __file__))
+os.chdir(dirname)
 
 def show_img_with_matplotlib(color_img, title, pos):
     """Shows an image using matplotlib capabilities"""
